6d6673b5b49341d43a1f6b231770e321f93763ed
Helen Hood <helen.c.hood@gmail.com>
2013-12-18 08:38:32 -0500

    tests for optional presence of rating in Book model, and that it's b/w 0 and 100.  Test passes.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ActiveRecord::Base
   validates_presence_of :title
+  validates :rating, inclusion: (0..100), allow_blank: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,4 +4,20 @@ describe Book do
   let(:blanks) { ['', nil] }
   it { should have_valid(:title).when('A Tale of Two Cities')}
   it { should_not have_valid(:title).when(*blanks)}
+
+  it 'should optionally have a rating' do
+    valid_book1 = Book.new(title: 'A Tale of Two Cities')
+    valid_book2 = Book.new(title: 'A Tale of Two Cities', rating: 87)
+
+    expect(valid_book1).to be_valid
+    expect(valid_book2).to be_valid
+  end
+
+  it 'should optionally have a rating between 0 and 100' do
+    invalid_book1 = Book.new(title: 'A Tale of Two Cities', rating: 135)
+    invalid_book2 = Book.new(title: 'A Tale of Two Cities', rating: -20)
+
+    expect(invalid_book1).to_not be_valid
+    expect(invalid_book2).to_not be_valid
+  end
 end
