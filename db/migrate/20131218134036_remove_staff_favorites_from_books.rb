9c1960f9f70fe22e09f5e87045821bbb65190218
Helen Hood <helen.c.hood@gmail.com>
2013-12-18 08:41:42 -0500

    step 5: removed staff_favorite column from books

---
+++ db/migrate/20131218134036_remove_staff_favorites_from_books.rb
@@ -0,0 +1,9 @@
+class RemoveStaffFavoritesFromBooks < ActiveRecord::Migration
+  def up
+    remove_column :books, :staff_favorite
+  end
+
+  def down
+    add_column :books, :staff_favorite, :boolean, default: false
+  end
+end
