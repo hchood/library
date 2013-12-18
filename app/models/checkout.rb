13d04f431a94069f48cd2599238abbd7fdc6b8ee
Helen Hood <helen.c.hood@gmail.com>
2013-12-18 11:31:33 -0500

    Checkout model passing all tests except 1 due_date validation check

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,3 +1,5 @@
 class Checkout < ActiveRecord::Base
   belongs_to :book
+  validates_presence_of :reader_name, :due_date, :book_id
+  validates :due_date, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
 end
--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe Checkout do
-  it { should have_valid(:reader_name) }
-  it { should have_valid(:book_id) }
-  it { should have_valid(:due_date) }
-  # the above tests pass simply w/ null constraints on the data, no validations
+  let(:blanks) { ['', nil] }
+  let(:invalid_dates) { ['dec 3', '12/2/13', '2013/11/20', '2013-1-5'] }
+  let(:book) { Book.create(title: 'A Tale of Two Cities')}
+
+  it { should have_valid(:reader_name).when('Helen Hood') }
+  it { should_not have_valid(:reader_name).when(*blanks) }
+
+  it { should have_valid(:book_id).when(book.id) }  # Does this test really do anything?  It passes without any validation in my model
+  it { should_not have_valid(:book_id).when(*blanks) }
+
+  it { should have_valid(:due_date).when('2013-11-03', '2013-04-05') }
+  it { should_not have_valid(:due_date).when(*invalid_dates) }
+  it { should_not have_valid(:due_date).when(*blanks) }
 
   it { should belong_to :book }
 end
