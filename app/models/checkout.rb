36871d08a3d1fa79a00e0f50feb06991ec4428a2
Helen Hood <helen.c.hood@gmail.com>
2013-12-20 09:00:34 -0500

    Modified tests and Checkout model to use validates_timeliness gem.  The test wasn't working before because the data type was a date, not a string, but I was using a regex to validate it.

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,5 +1,5 @@
 class Checkout < ActiveRecord::Base
   belongs_to :book
   validates_presence_of :reader_name, :due_date, :book_id
-  validates :due_date, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
+  validates_date :due_date, after: lambda { Date.current }
 end
--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe Checkout do
   let(:blanks) { ['', nil] }
-  let(:invalid_dates) { ['dec 3', '12/2/13', '2013/11/20', '2013-1-5'] }
+  let(:invalid_dates) { ['dec 3', 2013, '12/25/2013', Date.new(2013,12,3), 1.month.ago] }
+  let(:valid_dates) { [7.days.from_now, 1.month.from_now] }
   let(:book) { Book.create(title: 'A Tale of Two Cities')}
 
   it { should have_valid(:reader_name).when('Helen Hood') }
@@ -11,7 +12,7 @@ describe Checkout do
   it { should have_valid(:book_id).when(book.id) }  # Does this test really do anything?  It passes without any validation in my model
   it { should_not have_valid(:book_id).when(*blanks) }
 
-  it { should have_valid(:due_date).when('2013-11-03', '2013-04-05') }
+  it { should have_valid(:due_date).when(*valid_dates) }
   it { should_not have_valid(:due_date).when(*invalid_dates) }
   it { should_not have_valid(:due_date).when(*blanks) }
 
